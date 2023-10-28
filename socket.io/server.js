11570cfab3bb251233ffdec40880ae7ee6d0fc63
salehkarimy <salehkarimi423@gmail.com>
2023-10-28 18:55:01 +0330

    socket.io: send data to server and get data on server

--- socket.io/server.js
+++ socket.io/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3200;
+const { createServer } = require("node:http");
 const path = require("path");
+const { Server } = require("socket.io");
+
+const port = 3200;
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/view/index.html"));
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  socket.on("messenger", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+server.listen(port, () => {
   console.log(`server is running on port ${port}`);
   console.log(`http://localhost:${port}`);
 });
